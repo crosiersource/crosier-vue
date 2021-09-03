93066fd00eaf4e284158c40a84bbabbc8f860733
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-09-03 12:18:18 -0300

    Testando 0.0.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,17 @@
 module.exports = {
-  ignorePatterns: ["vendor/**/*"],
   root: true,
   env: {
-    browser: true,
+    node: true
   },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
   parserOptions: {
-    parser: "babel-eslint",
-    sourceType: "module",
+    parser: 'babel-eslint'
   },
-  extends: ["airbnb-base", "plugin:vue/vue3-essential", "prettier/vue", "plugin:prettier/recommended"],
   rules: {
-    "prefer-destructuring": [
-      "error",
-      {
-        array: false,
-        object: false,
-      },
-      {
-        enforceForRenamedProperties: false,
-      },
-    ],
-    "no-param-reassign": "off",
-    "no-plusplus": "off",
-    "no-console": "off",
-    "comma-dangle": "off",
-    "class-methods-use-this": "off",
-    "import/no-unresolved": "off",
-    "import/extensions": "off",
-    "implicit-arrow-linebreak": "off",
-    "import/prefer-default-export": "off",
-    "prettier/prettier": ["error", { singleQuote: false, endOfLine: "auto" }],
-    "max-len": ["error", { code: 100, comments: 140 }],
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
---
+++ rollup.config.js
@@ -0,0 +1,134 @@
+import vue from 'rollup-plugin-vue';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+
+const fs = require('fs-extra');
+const path = require('path');
+
+let entries = [];
+
+let globalDependencies = {
+    'vue': 'Vue',
+    // 'primevue/api': 'primevue.api',
+    // 'primevue/ripple': 'primevue.ripple',
+    // 'primevue/utils': 'primevue.utils',
+    // 'primevue/button': 'primevue.button',
+    // 'primevue/inputtext': 'primevue.inputtext',
+    // 'primevue/virtualscroller': 'primevue.virtualscroller',
+    // 'primevue/dialog': 'primevue.dialog',
+    // 'primevue/paginator': 'primevue.paginator',
+    // 'primevue/confirmationeventbus': 'primevue.confirmationeventbus',
+    // 'primevue/toasteventbus': 'primevue.toasteventbus',
+    // 'primevue/overlayeventbus': 'primevue.overlayeventbus',
+    // 'primevue/useconfirm': 'primevue.useconfirm',
+    // 'primevue/usetoast': 'primevue.usetoast',
+    // 'primevue/progressbar': 'primevue.progressbar',
+    // 'primevue/message': 'primevue.message',
+    // 'primevue/dropdown': 'primevue.dropdown',
+    // 'primevue/inputnumber': 'primevue.inputnumber',
+    // 'primevue/menu': 'primevue.menu',
+    // 'primevue/tieredmenu': 'primevue.tieredmenu',
+    // 'primevue/tree': 'primevue.tree',
+    // '@fullcalendar/core': 'FullCalendar'
+}
+
+function addEntry(folder, inFile, outFile) {
+    entries.push({
+        input: 'src/components/' + folder + '/' + inFile,
+        output: [
+            {
+                format: 'cjs',
+                file: 'dist/' + folder + '/' + outFile + '.cjs.js',
+            },
+            {
+                format: 'esm',
+                file: 'dist/' + folder + '/' + outFile + '.esm.js'
+            },
+            {
+                format: 'iife',
+                name: 'primevue.' + folder,
+                file: 'dist/' + folder + '/' + outFile + '.js',
+                globals: globalDependencies
+            }
+        ],
+        plugins: [
+            vue(),
+            postcss()
+        ]
+    });
+
+    entries.push({
+        input: 'src/components/' + folder + '/' + inFile,
+        output: [
+            {
+                format: 'cjs',
+                file: 'dist/' + folder + '/' + outFile + '.cjs.min.js'
+            },
+            {
+                format: 'esm',
+                file: 'dist/' + folder + '/' + outFile + '.esm.min.js'
+            },
+            {
+                format: 'iife',
+                name: 'primevue.' + folder,
+                file: 'dist/' + folder + '/' + outFile + '.min.js',
+                globals: globalDependencies
+            }
+        ],
+        plugins: [
+            vue(),
+            postcss(),
+            terser()
+        ]
+    });
+}
+
+function addSFC() {
+    fs.readdirSync(path.resolve(__dirname, './src/components/')).forEach(folder => {
+        fs.readdirSync(path.resolve(__dirname, './src/components/' + folder)).forEach(file => {
+            let name = file.split(/(.vue)$|(.js)$/)[0].toLowerCase();
+            if (/\.vue$/.test(file) && (name === folder)) {
+                addEntry(folder, file, name);
+            }
+        });
+    });
+}
+
+function addDirectives() {
+    // addEntry('badgedirective', 'BadgeDirective.js', 'badgedirective');
+    // addEntry('ripple', 'Ripple.js', 'ripple');
+    // addEntry('tooltip', 'Tooltip.js', 'tooltip');
+    // addEntry('styleclass', 'StyleClass.js', 'styleclass');
+}
+
+function addConfig() {
+    // addEntry('config', 'PrimeVue.js', 'config');
+}
+
+function addUtils() {
+    // addEntry('utils', 'Utils.js', 'utils');
+}
+
+function addApi() {
+    // addEntry('api', 'Api.js', 'api');
+}
+
+function addServices() {
+    // addEntry('confirmationservice', 'ConfirmationService.js', 'confirmationservice');
+    // addEntry('confirmationeventbus', 'ConfirmationEventBus.js', 'confirmationeventbus');
+    // addEntry('useconfirm', 'UseConfirm.js', 'useconfirm');
+    // addEntry('toastservice', 'ToastService.js', 'toastservice');
+    // addEntry('toasteventbus', 'ToastEventBus.js', 'toasteventbus');
+    // addEntry('overlayeventbus', 'OverlayEventBus.js', 'overlayeventbus');
+    // addEntry('usetoast', 'UseToast.js', 'usetoast');
+    // addEntry('terminalservice', 'TerminalService.js', 'terminalservice');
+}
+
+addSFC();
+addDirectives();
+addConfig();
+addUtils();
+addApi();
+addServices();
+
+export default entries;
